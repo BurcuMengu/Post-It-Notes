0a367d8823239dfe9a0181e723dc9d8c8e0cc4eb
BurcuMengu <burcumengu@gmail.com>
2024-11-17 16:20:20 +0300

    update

---
+++ src/client/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import CreateArea from "./CreateArea";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App(){
+    return (
+        <div>
+            <Footer />
+        </div>
+    )
+}
+
+
+export default App;
---
+++ src/server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import bcrypt from "bcrypt";
+import passport from "passport";
+import { Strategy } from "passport-local";
+import session from "express-session";
+import GoogleStrategy from "passport-google-oauth2";
+import env from "dotenv";
+
+const  app = express();
+const port = process.env.REACT_APP_PORT;
+const saltRounds = 10;
+env.config();
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
